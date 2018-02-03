689ebd70b5b6b16c814b7f510167dd429e2acec4
egirlasm <egirlasm@gmail.com>
2018-02-03 11:01:33 +0900

    first commi

---
+++ window.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from Tkinter import *
+class Application(Frame):
+    def say_hi(self):
+        print "hi there, everyone!"
+
+    def createWidgets(self):
+        self.QUIT = Button(self)
+        self.QUIT["text"] = "QUIT"
+        self.QUIT["fg"]   = "red"
+        self.QUIT["command"] =  self.quit
+
+        self.QUIT.pack({"side": "right"})
+
+        self.hi_there = Button(self)
+        self.hi_there["text"] = "Hello",
+        self.hi_there["command"] = self.say_hi
+
+        self.hi_there.pack({"side": "left"})
+
+        self.nima = Button(self)
+        self.nima["text"] = "fuck"
+        self.nima["command"] = self.say_hi
+        self.nima.pack({"side":"right"})
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+root = Tk()
+app = Application(master=root)
+app.mainloop()
+root.destroy()
